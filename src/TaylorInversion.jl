ee0ae06292e986256de05f2b340acb6e8dc49176
KeithWM <keith@myerscough.nl>
2023-01-20 20:26:12 +0100

    Working! And tested.

--- src/TaylorInversion.jl
+++ src/TaylorInversion.jl
@@ -1,5 +1,31 @@
 module TaylorInversion
+using Symbolics: @variables
+using Symbolics: Num, build_function
+using Symbolics: solve_for, expand, simplify, substitute
 
-greet() = print("Hello World!")
+function create_expressions(n::Int)
+    @variables x, y, z, a[1:n], A[1:n]
+    B = zeros(Num, n)
+
+    y = mapreduce(kan -> kan[2] * x^kan[1], +, enumerate(a))
+    subbed = substitute(y, Dict(x => mapreduce(kAn -> kAn[2] * z^kAn[1], +, enumerate(A)))) |> expand
+
+    subz = substitute(subbed, Dict(z => 0))
+    subz == 0 || error("Function should be at least linear in z")
+
+    # fp = getfp()
+    for i in 1:n
+        @info "Expanding term $i to create ivnersion expression"
+        divided = simplify(subbed / z)
+        subz = substitute(divided, Dict(z => 0))
+        B[i] = solve_for(subz ~ i == 1 ? 1 : 0, A[i]) |> simplify
+
+        subbed = substitute(divided - subz, Dict(A[i] => B[i]))
+    end
+
+    f = build_function(B, [a])[1] |> eval
+    @show B
+    return f
+end
 
 end # module TaylorInversion
--- test/rules.jl
+++ test/rules.jl
@@ -1,9 +1,33 @@
 using SymbolicUtils
+using SymbolicUtils: Chain, RestartedChain, Fixpoint, Prewalk
 
-@syms x::Complex y::Complex z::Complex
+@syms x::Real y::Real z::Real
 
-sqexpand = @rule (~x + ~y + ~z)/(~y) => (~x + ~z)/(~y) + 1
-# sqexpand = @rule (~x + ~y + ~z)/(~y) => (~x + ~z)/(~y) + 1
 
-# sqexpand(cos(y) + x / (x))
-sqexpand((x + y + z)/y)
+@syms a b c d e f g
+
+function getrule(m::Int)
+    r1 = @acrule ~b * x^m + ~c * x^m => (~b + ~c) * x^m
+    # r1 = @acrule ~~a1 + ~b * x^m + ~~a2 + ~c * x^m => sum(~~a1) + (~b + ~c) * x^m;
+    # r2 = @acrule ~~a1 + ~b * x^m + ~~a2 + ~c * x^m + ~~a3::(y->length(y) > 0) => sum(~~a1) + (~b + ~c) * x^m + sum(~~a3);
+    r3 = @acrule ~~a + ~b * x^m +  ~  ~ c => ~b * x^x
+    return Fixpoint(RestartedChain([r1])), r3
+end
+
+# fp, r = getrule(n)
+rfp = @acrule ~b * x^~n + ~c * x^~n => (~b + ~c) * x^~n;
+rfp1 = @acrule ~b * x + ~c * x => (~b + ~c) * x;
+fp = Fixpoint(Chain([rfp, rfp1]))
+
+@show fp(a + b * x^2 + c * x^1 + d)
+@show fp(b * x^1 + c + d * x^1)
+@show fp(b * x^2 + c + d * x^2 + e + f)
+@show fp(b * x^2 + c + b)
+@show fp(a + b * x^3 + c * x^2 + d + e + f * x^3 + g * x^2)
+
+rnb = @acrule (~~a + ~b * x^~n) / x => sum(~~a) / x + ~b * x^(~n - 1)
+rn1 = @acrule (~~a + x^~n) / x => sum(~~a) / x + x^(~n - 1)
+r1b = @acrule (~~a + ~b * x) / x => sum(~~a) / x + ~b
+r11 = @acrule (~~a + x) / x => sum(~~a) / x + 1
+fp = Fixpoint(Prewalk(Chain([rnb, rn1, r1b, r11])))
+@show fp((a * x^2 + x + b * x^3) / x + 1 + (b * x^2 + b * x) / x)
