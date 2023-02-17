21a43540334a81815f93ab02f0d3ff4b56144a00
KeithWM <keith@myerscough.nl>
2023-02-17 23:21:18 +0100

    Trimmed some unused things from the runtests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,7 @@
 using ReTest
 
 using TaylorInversion
-# (@__DIR__) in LOAD_PATH || push!(LOAD_PATH, @__DIR__)
-# @show LOAD_PATH
+
 include("TestTaylorInversion.jl")
-# using TestTaylorInversion
 
 retest(TaylorInversion, TestTaylorInversion)
-retest("Truncatedpower time")
